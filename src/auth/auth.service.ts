796ec6f9669197f1680112f9d88e53aae73452c3
NamigGuliyef <quliyevnamiq8@gmail.com>
2024-01-24 17:19:54 +0400

    signup changed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,10 +15,17 @@ export class AuthService {
   async signUp(createUserDto: CreateUserDto, files: { profilePhoto: Express.Multer.File[], driverLicensePhoto: Express.Multer.File[], carTechnicalPassportPhoto: Express.Multer.File }): Promise<userSignUpResponse> {
     const userExist = await this.userModel.findOne({ email: createUserDto.email })
     if (userExist) throw new HttpException('User already exists', HttpStatus.CONFLICT)
-    await this.userModel.create({ ...createUserDto, password: hashPassword, files })
-    return { message: 'You have registered successfully' }
+    const courierNameSurnameExist=await this.userModel.find({courierName:createUserDto.courierName,courierSurname:createUserDto.courierSurname})
+    if(courierNameSurnameExist){
+      const nameSurnameExist = createUserDto.courierName.slice(0,1).toLowerCase()+createUserDto.courierSurname.toLowerCase()+courierNameSurnameExist.length
+      await this.userModel.create({ ...createUserDto, username: nameSurnameExist, password: hashPassword, files })
+      return { message: 'You have registered successfully' }
+    } else {
+      const nameSurname = createUserDto.courierName.slice(0,1).toLowerCase()+createUserDto.courierSurname.toLowerCase()
+      await this.userModel.create({ ...createUserDto, username: nameSurname, password: hashPassword, files })
+      return { message: 'You have registered successfully' }
+    }
   }
-
 
 
 }
