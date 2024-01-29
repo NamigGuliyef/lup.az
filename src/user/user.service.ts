ffe6fefd7726503058e17a37a3fe1051918fe3a6
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2024-01-30 00:53:19 +0400

    user profile update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { messageResponse } from 'src/admin/admin.types';
+import cloudinary from 'src/config/cloudinary';
 import { tokenRequestType } from 'src/middleware/tokenReqType';
 import { CreateSupportDto } from 'src/notification/dto/notification.dto';
 import { UpdateUserDto } from './dto/user.dto';
@@ -16,14 +18,48 @@ export class UserService {
 
 
   // user update profile
-  // async updateProfile(updateUserDto: UpdateUserDto, files: { profilePhoto: Express.Multer.File[], driverLicensePhoto: Express.Multer.File[], carTechnicalPassportPhoto: Express.Multer.File[] }): Promise<User> {
-  //   if ((files.profilePhoto && files.profilePhoto[0] && files.profilePhoto[0].path) || 
+  async updateProfile(_id: string, updateUserDto: UpdateUserDto, files: { profilePhoto: Express.Multer.File[], driverLicensePhoto: Express.Multer.File[], carTechnicalPassportPhoto: Express.Multer.File[] }): Promise<messageResponse> {
+    const userExist = await this.userModel.findById(_id)
+    if (!userExist) throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    if ((files.profilePhoto && files.profilePhoto[0] && files.profilePhoto[0].path) || (files.driverLicensePhoto && files.driverLicensePhoto[0] && files.driverLicensePhoto[0].path) || (files.carTechnicalPassportPhoto && files.carTechnicalPassportPhoto[0] && files.carTechnicalPassportPhoto[0].path)) {
+      let profilePhoto = []
+      let driverLicensePhoto = []
+      let carTechnicalPassportPhoto = []
+      // eger profile photo varsa
+      if ((files.profilePhoto && files.profilePhoto[0] && files.profilePhoto[0].path)) {
+        for (let i = 0; i < files.profilePhoto.length; i++) {
+          const data = await cloudinary.uploader.upload(files.profilePhoto[i].path, { public_id: files.profilePhoto[i].originalname })
+          profilePhoto.push(data.url)
+        }
+        await this.userModel.findByIdAndUpdate(_id, { $set: { ...updateUserDto, profilePhoto } }, { new: true })
+        return { message: "User profile photo update" }
+      }
 
 
+      // eger driver license photo varsa
+      if ((files.driverLicensePhoto && files.driverLicensePhoto[0] && files.driverLicensePhoto[0].path)) {
+        for (let i = 0; i < files.driverLicensePhoto.length; i++) {
+          const data = await cloudinary.uploader.upload(files.driverLicensePhoto[i].path, { public_id: files.driverLicensePhoto[i].originalname })
+          driverLicensePhoto.push(data.url)
+        }
+        await this.userModel.findByIdAndUpdate(_id, { $set: { ...updateUserDto, driverLicensePhoto } }, { new: true })
+        return { message: "User driver license photo update" }
+      }
 
 
+      // eger car Technical Passport Photoo varsa 
+      if ((files.carTechnicalPassportPhoto && files.carTechnicalPassportPhoto[0] && files.carTechnicalPassportPhoto[0].path)) {
+        for (let i = 0; i < files.carTechnicalPassportPhoto.length; i++) {
+          const data = await cloudinary.uploader.upload(files.carTechnicalPassportPhoto[i].path, { public_id: files.carTechnicalPassportPhoto[i].originalname })
+          carTechnicalPassportPhoto.push(data.url)
+        }
+        await this.userModel.findByIdAndUpdate(_id, { $set: { ...updateUserDto, carTechnicalPassportPhoto } }, { new: true })
+        return { message: "User car chnical Passport photo photo update" }
+      }
 
-  // }
+    }
+
+  }
 
 
 
