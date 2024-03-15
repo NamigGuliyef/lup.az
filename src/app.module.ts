c7547473ba224ea7b0f6511e92ead27c2842c5ec
Rufat Aliyev <rufat845@gmail.com>
2024-03-15 22:11:56 +0400

    guest controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,17 @@ import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { SubfleetModule } from './subfleet/subfleet.module';
 import { SubfleetController } from './subfleet/subfleet.controller';
+import { GuestModule } from './guest/guest.module';
 
 @Module({
-  imports: [AuthModule, UserModule, MongooseModule.forRoot(uri), AdminModule, SubfleetModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    MongooseModule.forRoot(uri),
+    AdminModule,
+    SubfleetModule,
+    GuestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
